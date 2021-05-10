96d22a9cf0048eb2c08c924e4d5a28fcff6bdbc2
Pangeran Muhammad Waliyullah <pangeranwaliyullah@gmail.com>
2021-05-10 15:10:17 +0800

    First Commit

---
+++ home.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/vendor/owlcarousel/owl.carousel.css">
+    <link rel="stylesheet" href="assets/vendor/owlcarousel/owl.theme.default.css">
+    <title>Indonesian Scenery Craft</title>
+</head>
+<body style="background-color: var(--bg);">
+    
+    <?php include 'include/navbar.php'?>
+    <div class="hero" style="background-image: url('assets/images/hero-img.png');">
+        <div class="mx-auto my-auto d-flex flex-column">
+            <h1 class="h1">
+                Best Journey <br>
+                From Indonesia
+            </h1>
+            <button class="btn mx-auto px-5 py-3 fs-4">Explore</button>
+        </div>
+    </div>
+    <div class="container my-5">
+        <h4 class="text-center mb-4">Culture From Provincial Capitals</h4>
+        <div class="row mt-3">
+            <div class="owl-carousel owl-theme">
+                <div class="card-1 d-flex flex-column mt-3">
+                    <div class="card-img mx-auto mt-3" style="background-image: url(assets/images/DSC00169.jpg);"></div>
+                    <div class="card-text text-center">
+                        Lombok
+                    </div>
+                </div>
+                <div class="card-1 d-flex flex-column mt-3">
+                    <div class="card-img mx-auto mt-3" style="background-image: url(assets/images/DSC00169.jpg);"></div>
+                    <div class="card-text text-center">
+                        Jakarta
+                    </div>
+                </div>
+                <div class="card-1 d-flex flex-column mt-3">
+                    <div class="card-img mx-auto mt-3" style="background-image: url(assets/images/DSC00169.jpg);"></div>
+                    <div class="card-text text-center">
+                        Jakarta
+                    </div>
+                </div>
+                <div class="card-1 d-flex flex-column mt-3">
+                    <div class="card-img mx-auto mt-3" style="background-image: url(assets/images/DSC00169.jpg);"></div>
+                    <div class="card-text text-center">
+                        Jakarta
+                    </div>
+                </div>
+                <div class="card-1 d-flex flex-column mt-3">
+                    <div class="card-img mx-auto mt-3" style="background-image: url(assets/images/DSC00169.jpg);"></div>
+                    <div class="card-text text-center">
+                        Jakarta
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h4 class="my-4">Recommended Destinations</h4>
+        <div class="row my-3 justify-content-center align-items-center">
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <a href="">
+                    <div class="card-recommendation mt-3" style="background-image: url(assets/images/DSC00169.jpg);">
+                        <div class="star"><img src="assets/images/ic-starlogo.svg" alt=""> 5 Stars</div>
+                    </div>
+                    <div class="text-destination">
+                        Rinjani Mountain
+                    </div>
+                    <div class="text-location">
+                        NTB
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <a href="">
+                    <div class="card-recommendation mt-3" style="background-image: url(assets/images/DSC00169.jpg);">
+                        <div class="star"><img src="assets/images/ic-starlogo.svg" alt=""> 5 Stars</div>
+                    </div>
+                    <div class="text-destination">
+                        Rinjani Mountain
+                    </div>
+                    <div class="text-location">
+                        NTB
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <a href="">
+                    <div class="card-recommendation mt-3" style="background-image: url(assets/images/DSC00169.jpg);">
+                        <div class="star"><img src="assets/images/ic-starlogo.svg" alt=""> 5 Stars</div>
+                    </div>
+                    <div class="text-destination">
+                        Rinjani Mountain
+                    </div>
+                    <div class="text-location">
+                        NTB
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <a href="">
+                    <div class="card-recommendation mt-3" style="background-image: url(assets/images/DSC00169.jpg);">
+                        <div class="star"><img src="assets/images/ic-starlogo.svg" alt=""> 5 Stars</div>
+                    </div>
+                    <div class="text-destination">
+                        Rinjani Mountain
+                    </div>
+                    <div class="text-location">
+                        NTB
+                    </div>
+                </a>
+            </div>
+        </div>
+        <h4 class="text-center my-4">Most Rated Crafts</h4>
+        <div class="row mt-3 justify-content-center align-items-center">
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <a href="">
+                    <div class="card-craft mt-3" style="background-image: url(assets/images/ketak.jpeg);">
+                    </div>
+                    <div class="rating">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                    </div>
+                    <div class="text-craft">
+                        Tenun Songket
+                    </div>
+                    <div class="text-price">
+                        Rp. 200.000
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <a href="">
+                    <div class="card-craft mt-3" style="background-image: url(assets/images/songket.jpeg);">
+                    </div>
+                    <div class="rating">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                    </div>
+                    <div class="text-craft">
+                        Songket
+                    </div>
+                    <div class="text-price">
+                        Rp. 200.000
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <a href="">
+                    <div class="card-craft mt-3" style="background-image: url(assets/images/ketak.jpeg);">
+                    </div>
+                    <div class="rating">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                    </div>
+                    <div class="text-craft">
+                        Tenun Songket
+                    </div>
+                    <div class="text-price">
+                        Rp. 200.000
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <a href="">
+                    <div class="card-craft mt-3" style="background-image: url(assets/images/songket.jpeg);">
+                    </div>
+                    <div class="rating">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                        <img src="assets/images/ic-greystar.svg" alt="">
+                    </div>
+                    <div class="text-craft">
+                        Songket
+                    </div>
+                    <div class="text-price">
+                        Rp. 200.000
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <script src="assets/vendor/jquery.min.js"></script>
+    <script src="assets/vendor/owlcarousel/owl.carousel.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script>
+        window.addEventListener("scroll", function(){
+            var navbar = document.querySelector("nav");
+            navbar.classList.toggle("main-color", window.scrollY > 20);
+        });
+
+        $(document).ready(function(){
+            $(".owl-carousel").owlCarousel({
+                margin:20,
+                nav:true,
+                dots:false,
+                responsiveClass:true,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:3
+                    },
+                    1000:{
+                        items:4
+                    }
+                },
+                navText: ["<img src='assets/images/prev.png'>","<img src='assets/images/next.png'>"]
+            });
+        });
+    </script>
+</body>
+</html>
