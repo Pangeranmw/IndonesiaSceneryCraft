e74b97d15a0dde45604f73133a185b922de7aaf3
Pangeran Muhammad Waliyullah <pangeranwaliyullah@gmail.com>
2021-06-11 23:13:16 +0800

    make it better

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,10 +1,10 @@
 <?php
 class Controller{
   public function view($view, $data = []) {
-    require_once '/views/' . $view . '.php';
+    require_once '../app/views/' . $view . '.php';
   }
   public function model($model) {
-    require_once '/models/' . $model . '.php';
+    require_once '../app/models/' . $model . '.php';
     return new $model;
   }
 }
