25c04fef1d1898871c395013fa813573123f7602
Pangeran Muhammad Waliyullah <pangeranwaliyullah@gmail.com>
2021-06-12 00:19:38 +0800

    make it better

--- app/init.php
+++ app/init.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'core/App.php';
-require_once 'core/Controller.php';
-require_once 'core/Database.php';
-require_once 'config/Config.php';
+require_once ('core/App.php');
+require_once ('core/Controller.php');
+require_once ('core/Database.php');
+require_once ('config/Config.php');
